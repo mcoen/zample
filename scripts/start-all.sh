d88e71dd3061598dba17852c3ca41875e4e3369c
Mike Coen <michael.coen@gmail.com>
2026-05-03 20:40:53 -0400

    Fix AWS startup port collision between gateway and web

--- scripts/start-all.sh
+++ scripts/start-all.sh
@@ -14,10 +14,10 @@ cleanup() {
 
 trap cleanup EXIT INT TERM
 
-export PORT="${PORT:-3000}"
-export API_PORT="${API_PORT:-4000}"
-export LAUNCHES_PORT="${LAUNCHES_PORT:-4101}"
-export TASKS_PORT="${TASKS_PORT:-4102}"
+WEB_PORT="${PORT:-3000}"
+GATEWAY_PORT="${API_PORT:-4000}"
+LAUNCHES_PORT="${LAUNCHES_PORT:-4101}"
+TASKS_PORT="${TASKS_PORT:-4102}"
 
 node services/launches-service/src/index.js &
 pids+=("$!")
@@ -25,10 +25,13 @@ pids+=("$!")
 node services/tasks-service/src/index.js &
 pids+=("$!")
 
+PORT="$GATEWAY_PORT" \
+LAUNCHES_SERVICE_URL="http://127.0.0.1:${LAUNCHES_PORT}" \
+TASKS_SERVICE_URL="http://127.0.0.1:${TASKS_PORT}" \
 node services/api-gateway/src/index.js &
 pids+=("$!")
 
-npm run start --workspace @zample/web &
+PORT="$WEB_PORT" npm run start --workspace @zample/web &
 pids+=("$!")
 
 wait -n
